2f1e1cbb6268a1f711b8414730807e6ea1ac1726
Ahmed Mughram <contact@vszlx.com>
2026-05-13 15:02:05 +0300

    testing with loops

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import time
+
 class XORShift32():
   def __init__(self, seed = 1):
     self.state = seed
@@ -9,11 +11,35 @@ class XORShift32():
     x ^= (x >> 17)
     x ^= (x << 5) & 0xFFFFFFFF
 
-    self.state = x^2
+    self.state = x
 
     return self.state
 
+N = 100_000_000
+K = 1_000_000
+seed = 3701661588
 
-PRNG = XORShift32(3701661588)
-for i in range(150):
-  print(PRNG.random())
+PRNG = XORShift32(seed)
+start = time.time()
+
+for _ in range(K): PRNG.random()
+
+t = (time.time() - start) / K
+estimated_total = t * N
+
+print(f"Estimated time: {estimated_total:.2f} seconds")
+print(f"Estimated minutes: {estimated_total/60:.2f}")
+
+seen = set()
+
+for i in range(N):
+  state = PRNG.state
+
+  if state in seen:
+    print(f"Duplicate at step {i}: {state}")
+    break
+
+  seen.add(state)
+  PRNG.random()
+
+print("exit")
